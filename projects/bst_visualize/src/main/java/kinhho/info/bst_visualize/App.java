72bd0a58b96d0e727ba3547051ded5a21a0a1d19
HoNgocKinh <hongockinh69@gmail.com>
2022-10-27 10:34:52 +0700

    Git ignore module

---
+++ projects/bst_visualize/src/main/java/kinhho/info/bst_visualize/App.java
@@ -0,0 +1,13 @@
+package kinhho.info.bst_visualize;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
